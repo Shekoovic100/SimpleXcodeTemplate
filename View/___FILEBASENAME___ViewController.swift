e6cd2cd9bddf134f2ed03fde9b38b0c9250dce62
Sherif Samy <sherif.samy7770@gmail.com>
2022-12-20 14:48:53 +0200

    Xcode Template

---
+++ View/___FILEBASENAME___ViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class ___FILEBASENAMEASIDENTIFIER___: UIViewController {
+    
+    let viewModel: ___VARIABLE_productName___ViewModel
+    
+
+    override func viewDidLoad() {
+            super.viewDidLoad()
+            
+        }
+        
---
+++ ViewModel/___FILEBASENAME___ViewModel.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+
+class ___FILEBASENAMEASIDENTIFIER___ {
+
+    private let model: ___VARIABLE_productName:identifier___?
+     
+    var BindingClosureResult: (()->()) = {}
+    
+    init(withModel model: ___VARIABLE_productName:identifier___) {
+        self.model = model
+    }
+}
